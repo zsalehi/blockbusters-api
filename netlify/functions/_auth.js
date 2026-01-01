b5abb0dcd2e6e58e571d33b1d1e1ceda69d88506
Zaigham Salehi <zsalehi@Zaighams-MacBook-Pro.local>
2026-01-01 13:59:56 -0500

    Remove unused auth helper

--- netlify/functions/_auth.js
+++
@@ -1,6 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-// Supabase JWT secret is in Supabase project settings -> API -> JWT Settings.
-// But fastest approach: call Supabase to validate token by fetching user via Admin API.
-// We'll do that in the handlers using supabaseAdmin.auth.getUser(token).
-module.exports = {};
